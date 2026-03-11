dbc7abe3c39c9bc12fe2dde0408b640f5c8f2b4c
Bruno Ardenghi <bruno.samuel@ufrgs.br>
2026-03-11 12:26:11 -0300

    separate all generation steps into functions

--- src/LEEA.jl
+++ src/LEEA.jl
@@ -3,7 +3,7 @@ module LEEA
 import Lux
 using Random: Xoshiro, rand!
 using Polyester: @batch
-using StatsBase: Weights, sample
+using StatsBase: Weights, sample, sample!
 using Base.Threads: nthreads, threadid
 using ..Data: load_MNIST
 using ..Models: CNN_2C2D_MNIST
@@ -17,52 +17,73 @@ const logitcrossentropy = Lux.CrossEntropyLoss(; logits = Val(true))
     r::Float32 = 0.04   # mutation rate
     m::Float32 = 0.03   # mutation power
     γₘ::Float32 = 0.99  # mutation power decay
-    pₛ::Float32 = 0.4   # selection proportion
+    p::Float32 = 0.4    # selection proportion
     s::Float32 = 0.5    # sexual reproduction proportion
     γᵢ::Float32 = 0.2   # fitness inheritance decay
 end
 
-function mutate!(O, P, as_idx, rngs, U₁, U₂, Nₐ, r, m)
-    return @batch for i in 1:Nₐ
+function evaluate_population!(f, model, P, X, Y, st, N)
+    return @batch for j in 1:N
+        θⱼ = NamedTuple{(:params,)}((@view(P[:, j]),))
+        Ŷ, _ = model(X, θⱼ, st)
+        f[j] = 1.0f0 / (1.0f0 + logitcrossentropy(Ŷ, Y))
+    end
+end
+
+function select_parents!(f, pₐ, p₁, p₂, rng, N, p)
+    wheel = partialsortperm(f, 1:round(Int, p * N), rev = true)
+    weights = Weights(@view f[wheel])
+
+    sample!(rng, wheel, weights, pₐ)
+    sample!(rng, wheel, weights, p₁)
+    sample!(rng, wheel, weights, p₂)
+
+    collisions = p₁ .== p₂
+    while any(collisions)
+        n_collisions = sum(collisions)
+        p₂[collisions] .= sample(rng, wheel, weights, n_collisions)
+        collisions = p₁ .== p₂
+    end
+    return
+end
+
+function reproduce_assexual!(O, P, pₐ, U₁, U₂, rngs, Nₐ, r, m)
+    return @batch for j in 1:Nₐ
         tid = threadid()
         u₁ = U₁[tid]
         u₂ = U₂[tid]
+        pⱼ = pₐ[j]
 
         rand!(rngs[tid], u₁)
         rand!(rngs[tid], u₂)
 
-        p = as_idx[i]
-
-        @. O[:, i] = P[:, p] + (u₁ < r) * m * (2.0f0 * u₂ - 1.0f0)
+        @. O[:, j] = P[:, pⱼ] + (u₁ < r) * m * (2.0f0 * u₂ - 1.0f0)
     end
 end
 
-function reproduce!(O, P, wheel_idx, wheel_w, rngs, U, Nₛ, Nₐ)
-    return @batch for i in 1:Nₛ
+function reproduce_sexual!(O, P, p₁, p₂, U, rngs, Nₛ, Nₐ)
+    return @batch for j in 1:Nₛ
         tid = threadid()
         u = U[tid]
+        p₁ⱼ = p₁[j]
+        p₂ⱼ = p₂[j]
 
         rand!(rngs[tid], u)
 
-        p₁ = sample(rngs[tid], wheel_idx, wheel_w)
-        p₂ = sample(rngs[tid], wheel_idx, wheel_w)
-
-        while p₁ == p₂
-            p₂ = sample(rngs[tid], wheel_idx, wheel_w)
-        end
-
-        @. O[:, Nₐ + i] = ifelse(u < 0.5f0, P[:, p₁], P[:, p₂])
+        @. O[:, Nₐ + j] = ifelse(u < 0.5f0, P[:, p₁ⱼ], P[:, p₂ⱼ])
     end
 end
 
 function train_LEEA(; seed::Int, batchsize::Int, generations::Int)
+    (; N, r, m, γₘ, p, s, γᵢ) = LEEAConfig()
+
     n_threads = nthreads()
     rng = Xoshiro(seed)
     rngs = [Xoshiro(seed + t) for t in 1:n_threads]
-    (; N, r, m, γₘ, pₛ, s, γᵢ) = LEEAConfig()
 
     train_dataloader, _ = load_MNIST(; rng, batchsize, balanced = true)
     model = CNN_2C2D_MNIST
+
     _, st = Lux.setup(rng, model)
     st = Lux.testmode(st)
     θ_len = Lux.parameterlength(model)
@@ -73,32 +94,22 @@ function train_LEEA(; seed::Int, batchsize::Int, generations::Int)
     Nₛ = round(Int, s * N)
     Nₐ = N - Nₛ
 
-    f = Vector{Float32}(undef, N)
-    U₁ = [Vector{Float32}(undef, θ_len) for _ in 1:n_threads]
-    U₂ = [Vector{Float32}(undef, θ_len) for _ in 1:n_threads]
-    Uₛ = [Vector{Float32}(undef, θ_len) for _ in 1:n_threads]
+    alloc(T, n) = Vector{T}(undef, n)
+    f = alloc(Float32, N)
+    pₐ, p₁, p₂ = alloc(Int, Nₐ), alloc(Int, Nₛ), alloc(Int, Nₛ)
+    Uₛ, U₁, U₂ = ntuple(_ -> [alloc(Float32, θ_len) for _ in 1:n_threads], 3)
 
     for i in 1:generations
         X, Y = popfirst!(train_dataloader)
 
-        @batch for j in 1:N
-            θⱼ = NamedTuple{(:params,)}((@view(P[:, j]),))
-            Ŷ, _ = model(X, θⱼ, st)
-            f[j] = 1.0f0 / (1.0f0 + logitcrossentropy(Ŷ, Y))
-        end
-
-        wheel_idx = partialsortperm(f, 1:round(Int, pₛ * N), rev = true)
-        wheel_w = Weights(@view f[wheel_idx])
-
-        as_idx = sample(rng, wheel_idx, wheel_w, Nₐ)
-
-        mutate!(O, P, as_idx, rngs, U₁, U₂, Nₐ, r, m)
-        reproduce!(O, P, wheel_idx, wheel_w, rngs, Uₛ, Nₛ, Nₐ)
+        evaluate_population!(f, model, P, X, Y, st, N)
+        select_parents!(f, pₐ, p₁, p₂, rng, N, p)
+        reproduce_assexual!(O, P, pₐ, U₁, U₂, rngs, Nₐ, r, m)
+        reproduce_sexual!(O, P, p₁, p₂, Uₛ, rngs, Nₛ, Nₐ)
 
         m *= γₘ
         P, O = O, P
     end
-
     return
 end
 
